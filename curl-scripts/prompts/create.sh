9b3d5d2bc6ca89cc3f9f32e5bf6486f1a5f51075
torystosse <tory.mae.stosse@gmail.com>
2020-01-28 18:18:54 -0500

    Added prompts to server.js, created curl scripts
    Added promptRoutes to server.js
    Created all curl-scripts for prompts

---
+++ curl-scripts/prompts/create.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/prompts"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
+    "prompt": {
+      "text": "'"${TEXT}"'"
+    }
+  }'
+
+echo
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 // require route files
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
+const promptRoutes = require('./app/routes/prompt_routes')
 
 // require middleware
 const errorHandler = require('./lib/error_handler')
@@ -63,6 +64,7 @@ app.use(requestLogger)
 // register route files
 app.use(exampleRoutes)
 app.use(userRoutes)
+app.use(promptRoutes)
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
